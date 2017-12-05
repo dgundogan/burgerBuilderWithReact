d747bab52b0cb474ffcb95a4467fec1ce52b71f2
Dursun <dgundogan@gmail.com>
2017-12-05 22:36:50 +0000

    no message

--- burger-builder/src/hoc/Layout/Layout.js
+++ burger-builder/src/hoc/Layout/Layout.js
@@ -1,37 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
 import Aux from '../Aux/Aux';
 import classes from './Layout.css';
-import Toolbar from '../../Components/Navigation/Toolbar/Toolbar';
-import SideDrawer from '../../Components/Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
-
     state = {
         showSideDrawer: false
     }
 
-    SideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer: false});
-    };
-
-    sideDrawerToggleHandler = () => {
-        this.setState((prevState) => {
-            return  {showSideDrawer: !prevState.showSideDrawer}
-            }
-        );
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
     }
 
-    render (){
-        return ( 
-        <Aux>
-            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
-            <SideDrawer
-                open={this.state.showSideDrawer} 
-                closed={this.SideDrawerClosedHandler}/>
-            <main className={classes.Content}>
-                {this.props.children}
-            </main>
-        </Aux>);
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
     }
 }
 
